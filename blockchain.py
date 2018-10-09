d1529e79b6ce9e329658988f4170b3830490b0fc
Iampato <858wpwaweru@gmail.com>
2018-10-09 23:02:28 +0300

    added input function

--- blockchain.py
+++ blockchain.py
@@ -8,6 +8,8 @@ def add_blockchain(transaction_amount):
     blockchain.append(transaction_amount)
     
 print ("Last value: {}".format( get_last_blockchain_value() ))
-add_blockchain(10)
+
+tx_amnt = input('Please enter an amount')
+add_blockchain(tx_amnt)
 print("My current blockchain: {}".format( blockchain ))
 
