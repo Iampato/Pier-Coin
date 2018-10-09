216fa91e028a83f65a08822c05e5739983a0c60d
Iampato <858wpwaweru@gmail.com>
2018-10-09 22:55:27 +0300

    keeping track of last blockchain value

--- blockchain.py
+++ blockchain.py
@@ -4,8 +4,10 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 def add_blockchain(transaction_amount):
+
     blockchain.append(transaction_amount)
     
+print ( get_last_blockchain_value() )
 add_blockchain(10)
 print( blockchain )
 
