0b8eaad875e54f8830666862b9f11654778c8cc6
Iampato <858wpwaweru@gmail.com>
2019-06-10 10:05:10 +0300

    A new project structure

--- blockchain.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from data import Data
-
-#let the hacking begin
--- data.py
+++
@@ -1,7 +0,0 @@
-import struct
-
-class Data(object):
-  def __init__(self, dat):
-    self.dat = dat
-    self.ptr = 0
-  
